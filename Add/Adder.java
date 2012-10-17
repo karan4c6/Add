92b0ab69724ed15d8cf281806a157a2d29725c53
Karan Sharma <javakaran@live.com>
2012-10-17 10:21:09 +0530

    Folder in Folder
    Add folder in folder

---
+++ Add/Adder.java
@@ -0,0 +1,21 @@
+package adder;
+
+public class Adder 
+{
+
+    public static void main(String[] args)
+    {
+	int length = args.length;
+	if(length > 1)
+	{
+	    double sumOfArgs = 0.0;
+	    for(int i = 0; i<length; i++)
+	    {
+		sumOfArgs += Double.parseDouble(args[i]);
+	    }
+	    System.out.println(sumOfArgs);
+	}else {
+	    System.out.print("You entered only one value.\n");
+	}
+    }
+}
